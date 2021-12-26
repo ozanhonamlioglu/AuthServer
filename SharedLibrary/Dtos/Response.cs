b9b52ca27cc54f3fd1e9eae8611d553e0671b207
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2021-12-26 22:49:24 +0300

    Error and response dtos are added

---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+        public ErrorDto Error { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; } = false;
+
+        public static Response<T> Success(T data, int statusCode) => new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+
+        public static Response<T> Success(int statusCode) => new Response<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };
+
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode) => new Response<T> { Error = errorDto, StatusCode = statusCode };
+
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode };
+        }
+    }
+}
