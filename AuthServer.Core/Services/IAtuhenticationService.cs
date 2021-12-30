f4af087ab2d3c357c44a80ca523cf67099077fb0
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2021-12-31 00:33:45 +0300

    core layer is done

---
+++ AuthServer.Core/Services/IAtuhenticationService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAtuhenticationService
+    {
+        Task<Response<TokenDto>> CreateToken(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task <Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
