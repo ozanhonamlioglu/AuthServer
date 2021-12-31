983b96ac408e05648f883f2a09c198de6c3c6e50
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2021-12-31 16:57:06 +0300

    GenericRepository is coded

--- AuthServer.Data/AppDbContext.cs
+++ AuthServer.Data/AppDbContext.cs
@@ -12,10 +12,7 @@ namespace AuthServer.Data
 {
     public class AppDbContext: IdentityDbContext<UserApp, IdentityRole, string>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
-        {
-
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
 
         public DbSet<Product> Products { get; set;}
         public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
