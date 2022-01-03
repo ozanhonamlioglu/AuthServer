baceec746a9e8ef074969647d10f7af6085b97b7
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2022-01-03 08:11:54 +0300

    genericservice, options pattern, signservice

--- AuthServer.Core/Services/IGenericService.cs
+++ AuthServer.Core/Services/IGenericService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AuthServer.Core.Repositories.Services
 {
-    public interface IServiceGeneric<TEntity,TDto> where TEntity : class where TDto : class
+    public interface IGenericService<TEntity,TDto> where TEntity : class where TDto : class
     {
         Task<Response<TDto>> GetByIdAsync(int id);
 
@@ -16,10 +16,10 @@ namespace AuthServer.Core.Repositories.Services
 
         Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
 
-        Task<Response<TDto>> AddAsync(TEntity entity);
+        Task<Response<TDto>> AddAsync(TDto entity);
 
-        Task<Response<NoDataDto>> Remove(TEntity entity);
+        Task<Response<NoDataDto>> Remove(int id);
 
-        Task<Response<NoDataDto>> Update(TEntity entity);
+        Task<Response<NoDataDto>> Update(TDto entity, int id);
     }
 }
