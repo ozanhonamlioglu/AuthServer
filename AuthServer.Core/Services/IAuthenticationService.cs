941acac029e6884dd54fd961ddfbeaa7b152c937
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2022-01-05 07:04:55 +0300

    started to build authentication service

--- AuthServer.Core/Services/IAuthenticationService.cs
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AuthServer.Core.Services
 {
-    public interface IAtuhenticationService
+    public interface IAuthenticationService
     {
         Task<Response<TokenDto>> CreateToken(LoginDto loginDto);
         Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
---
+++ AuthServer.Service/Services/AuthenticationService.cs
@@ -0,0 +1,60 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+
+        private readonly List<Client> _clients;
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<UserRefreshToken> _userRefreshTokenService;
+
+        public AuthenticationService(IOptions<List<Client>> optionsClient, 
+            ITokenService tokenService, 
+            UserManager<UserApp> userManager, 
+            IUnitOfWork unitOfWork, 
+            IGenericRepository<UserRefreshToken> userRefreshToken)
+        {
+            _clients = optionsClient.Value;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _userRefreshTokenService = userRefreshToken;
+        }
+
+        public Task<Response<TokenDto>> CreateToken(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
