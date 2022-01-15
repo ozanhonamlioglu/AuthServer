4ea873b6fdd991807b6b0c9b3daf6c85840c96aa
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2022-01-15 13:56:00 +0300

    controllers are building

---
+++ AuthServer.API/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.API.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        public IActionResult ActionResultInstance<T>(Response<T> response) where T : class
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -111,7 +111,7 @@ namespace AuthServer.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
